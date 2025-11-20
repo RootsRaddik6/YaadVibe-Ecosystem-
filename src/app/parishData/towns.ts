7163094c9b431ba0bc9926b2e1caa53922d9064c
RootsRaddik6 <korjamltd@gmail.com>
2025-11-20 13:22:47 -0500

    Update towns.ts

--- src/app/parishData/towns.ts
+++ src/app/parishData/towns.ts
@@ -1,23 +1,24 @@
 // src/app/parishData/towns.ts
-
-export interface Town {
+export type TownsForParish = {
   parishCode: string;
   towns: string[];
-}
+};
 
-export const TOWNS: Town[] = [
-  { parishCode: "kingston", towns: ["Kingston", "Downtown", "New Kingston"] },
-  { parishCode: "st-andrew", towns: ["Half Way Tree", "Constant Spring", "Papine", "Manor Park"] },
-  { parishCode: "st-catherine", towns: ["Spanish Town", "Portmore", "Old Harbour"] },
-  { parishCode: "clarendon", towns: ["May Pen", "Chapelton", "Milk River"] },
+export const TOWNS: TownsForParish[] = [
+  { parishCode: "kingston", towns: ["Kingston", "Downtown", "New Kingston", "Port Royal"] },
+  { parishCode: "st-andrew", towns: ["Half Way Tree", "Mona", "Papine", "Constant Spring"] },
+  { parishCode: "st-catherine", towns: ["Spanish Town", "Portmore", "Old Harbour", "Linstead"] },
+  { parishCode: "clarendon", towns: ["May Pen", "Chapelton", "Frankfield"] },
   { parishCode: "manchester", towns: ["Mandeville", "Christiana", "Porus"] },
   { parishCode: "st-elizabeth", towns: ["Black River", "Santa Cruz", "Treasure Beach"] },
   { parishCode: "westmoreland", towns: ["Negril", "Savanna-la-Mar", "Little London"] },
-  { parishCode: "hanover", towns: ["Lucea", "Hopewell", "Green Island"] },
+  { parishCode: "hanover", towns: ["Lucea", "Hopewell"] },
   { parishCode: "st-james", towns: ["Montego Bay", "Rose Hall", "Ironshore"] },
-  { parishCode: "trelawny", towns: ["Falmouth", "Duncans", "Clarks Town"] },
-  { parishCode: "st-ann", towns: ["Ocho Rios", "St. Ann’s Bay", "Runaway Bay"] },
-  { parishCode: "st-mary", towns: ["Port Maria", "Annotto Bay", "Oracabessa"] },
-  { parishCode: "portland", towns: ["Port Antonio", "Boston", "Buff Bay"] },
-  { parishCode: "st-thomas", towns: ["Morant Bay", "Yallahs", "Seaforth"] },
+  { parishCode: "trelawny", towns: ["Falmouth", "Duncans", "Martha Brae"] },
+  { parishCode: "st-ann", towns: ["Ocho Rios", "St. Ann's Bay", "Bamboo"] },
+  { parishCode: "st-mary", towns: ["Port Maria", "Oracabessa", "Annotto Bay"] },
+  { parishCode: "portland", towns: ["Port Antonio", "Boston Bay", "Buff Bay"] },
+  { parishCode: "st-thomas", towns: ["Morant Bay", "Yallahs", "Bath"] },
 ];
+
+export default TOWNS;
