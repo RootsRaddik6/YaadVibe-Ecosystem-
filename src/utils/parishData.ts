ae39c4a5316745576752a54ad5da212027a91d66
RootsRaddik6 <korjamltd@gmail.com>
2025-11-20 11:13:05 -0500

    Update parishData.ts

--- src/utils/parishData.ts
+++ src/utils/parishData.ts
@@ -1,1 +1,11 @@
-ttt
+// src/utils/parishData.ts
+export const parishes = [
+  {
+    name: "Kingston",
+    towns: ["Kingston", "New Kingston"],
+    airport: "KIN",
+    attractions: ["Bob Marley Museum", "Devon House"],
+    hotels: ["Courtleigh", "AC Marriott", "Pegasus"],
+  },
+  // ... add others
+];
