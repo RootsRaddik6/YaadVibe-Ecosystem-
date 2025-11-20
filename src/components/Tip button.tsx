8e1b693b8acebffa98bb7f3c844bb4341b8cb518
RootsRaddik6 <korjamltd@gmail.com>
2025-11-19 21:51:16 -0500

    Create Tip button.tsx

---
+++ src/components/Tip button.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function TipButton() {
+  return (
+    <button className="text-3xl bg-black/60 rounded-full px-4 py-2 backdrop-blur border border-white/40 fixed bottom-6 right-6 z-50 shadow-xl">
+      🔥
+    </button>
+  );
+}
