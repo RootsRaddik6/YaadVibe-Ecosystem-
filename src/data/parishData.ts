572ddcdecb509e56ab4af8a66437be5cbc21e722
RootsRaddik6 <korjamltd@gmail.com>
2025-11-20 02:01:06 -0500

    Create parishData.ts

---
+++ src/data/parishData.ts
@@ -0,0 +1,72 @@
+// src/data/parishData.ts
+
+export const parishes = [
+  {
+    parish: "Kingston",
+    towns: ["Kingston", "New Kingston", "Cross Roads", "Downtown"],
+  },
+  {
+    parish: "St. Andrew",
+    towns: [
+      "Half Way Tree",
+      "Constant Spring",
+      "Papine",
+      "Liguanea",
+      "Manor Park",
+    ],
+  },
+  {
+    parish: "St. Catherine",
+    towns: [
+      "Spanish Town",
+      "Portmore",
+      "Old Harbour",
+      "Linstead",
+      "Ewarton",
+    ],
+  },
+  {
+    parish: "Clarendon",
+    towns: ["May Pen", "Milk River", "Lionel Town", "Race Course"],
+  },
+  {
+    parish: "Manchester",
+    towns: ["Mandeville", "Christiana", "Kendal"],
+  },
+  {
+    parish: "St. Elizabeth",
+    towns: ["Santa Cruz", "Black River", "Lacovia"],
+  },
+  {
+    parish: "Westmoreland",
+    towns: ["Savanna-la-Mar", "Negril", "Little London"],
+  },
+  {
+    parish: "Hanover",
+    towns: ["Lucea", "Hopewell", "Green Island"],
+  },
+  {
+    parish: "St. James",
+    towns: ["Montego Bay", "Ironshore", "Rose Hall"],
+  },
+  {
+    parish: "Trelawny",
+    towns: ["Falmouth", "Duncans", "Clarks Town"],
+  },
+  {
+    parish: "St. Ann",
+    towns: ["Ocho Rios", "Runaway Bay", "St. Ann's Bay"],
+  },
+  {
+    parish: "St. Mary",
+    towns: ["Port Maria", "Oracabessa", "Annotto Bay"],
+  },
+  {
+    parish: "Portland",
+    towns: ["Port Antonio", "Buff Bay", "Manchioneal"],
+  },
+  {
+    parish: "St. Thomas",
+    towns: ["Morant Bay", "Yallahs", "Port Morant"],
+  },
+];
