4ac096f386de10ac767d5d1f1763cb17db709b45
RootsRaddik6 <korjamltd@gmail.com>
2025-11-19 22:30:36 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [{ protocol: "https", hostname: "**" }]
-  }
+  experimental: {
+    serverActions: true,
+  },
 };
+
 module.exports = nextConfig;
