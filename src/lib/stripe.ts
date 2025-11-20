e339af2118de2d8d489b03f562815aa954049693
RootsRaddik6 <korjamltd@gmail.com>
2025-11-20 10:06:08 -0500

    Update stripe.ts

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,11 +1,10 @@
+// src/lib/stripe.ts
 import Stripe from "stripe";
 
-if (!process.env.STRIPE_SECRET) {
-  throw new Error("STRIPE_SECRET missing");
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error("Missing STRIPE_SECRET_KEY");
 }
 
-const stripe = new Stripe(process.env.STRIPE_SECRET, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2023-10-16",
 });
-
-export default stripe;
