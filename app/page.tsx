74198cc2e93c1705f7b9498ac483993b3c2dc04a
RootsRaddik6 <korjamltd@gmail.com>
2025-11-20 00:34:53 -0500

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40, fontSize: 24 }}>
+      <h1>YaadVibe Ecosystem is LIVE 🎉</h1>
+      <p>Root route is working.</p>
+    </div>
+  );
+}
