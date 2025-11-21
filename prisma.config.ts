4b2cc819fb684609c127e8211127fba5adaf192a
RootsRaddik6 <korjamltd@gmail.com>
2025-11-21 16:48:42 -0500

    Delete prisma.config.ts

--- prisma.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from '@prisma/config';
-
-export default defineConfig({
-  client: {
-    provider: "postgresql",
-    url: process.env.DATABASE_URL,
-    directUrl: process.env.DIRECT_URL,
-  }
-});
