42029bd225df4b65bc077dcd317f9f0c08db680c
RootsRaddik6 <korjamltd@gmail.com>
2025-11-21 15:33:07 -0500

    Create prisma.config.ts

---
+++ prisma.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from '@prisma/config';
+
+export default defineConfig({
+  client: {
+    provider: "postgresql",
+    url: process.env.DATABASE_URL!,
+    directUrl: process.env.DIRECT_URL!,
+  },
+  migrate: {
+    provider: "postgresql",
+    url: process.env.DATABASE_URL!,
+  }
+});
