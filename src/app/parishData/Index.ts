c7afda7694c3b55c91c1c42f0d308d32eec13446
RootsRaddik6 <korjamltd@gmail.com>
2025-11-22 22:55:30 -0500

    Update Index.ts

--- src/app/parishData/Index.ts
+++ src/app/parishData/Index.ts
@@ -3,6 +3,6 @@ export * from "./parishes";
 export * from "./towns";
 export * from "./hotels";
 export * from "./tours";
-export * from "./transport";
+export * from "./transportation";
 export * from "./flights";
 export * from "./attractions";
