71a7ecfbd3eb5c6d67bd1faea8738d7a25b7f54d
RootsRaddik6 <korjamltd@gmail.com>
2025-11-23 00:03:23 -0500

    Update ton.ts

--- src/utils/ton.ts
+++ src/utils/ton.ts
@@ -1,1 +1,7 @@
-ttt
+import { TonClient, WalletContractV4 } from "ton";
+import { mnemonicToPrivateKey } from "ton-crypto";
+
+export const tonClient = new TonClient({
+  endpoint: "https://testnet.toncenter.com/api/v2/jsonRPC",
+  apiKey: process.env.TON_API_KEY
+});
