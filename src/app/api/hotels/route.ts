148a943d691680f46d9c5c7be7d643980746be58
RootsRaddik6 <korjamltd@gmail.com>
2025-11-23 09:19:53 -0500

    Update route.ts

--- src/app/api/hotels/route.ts
+++ src/app/api/hotels/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { HOTELS } from "@/parishData";
+import { HOTELS } from "@/app/parishData/hotels";
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
