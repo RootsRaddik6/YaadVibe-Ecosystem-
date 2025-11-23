e2cb58ec87292344af6e465ecf5457d21e7935c5
RootsRaddik6 <korjamltd@gmail.com>
2025-11-23 00:04:27 -0500

    Update lynk.ts

--- src/utils/lynk.ts
+++ src/utils/lynk.ts
@@ -1,1 +1,13 @@
-ttt
+export async function sendJamDex(to: string, amount: number) {
+  return await fetch("https://api.lynk.com/jamdex/send", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${process.env.LYNK_API_KEY}`
+    },
+    body: JSON.stringify({
+      to,
+      amount
+    })
+  }).then(r => r.json());
+}
