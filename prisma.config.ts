c952e1d7aa450e17e33ece07ad0f428e09fb33b5
RootsRaddik6 <korjamltd@gmail.com>
2025-11-22 20:37:31 -0500

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from '@prisma/config';
 export default defineConfig({
   datasource: {
     provider: 'postgresql',
-    url: process.env.DATABASE_URL!,   // Make sure this is defined
+    url: process.env.DATABASE_URL,
   },
 });
