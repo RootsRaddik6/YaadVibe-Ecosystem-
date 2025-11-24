a3ce7eb2785d1334175656dd003f9d06c95cdb39
RootsRaddik6 <korjamltd@gmail.com>
2025-11-24 00:06:45 -0500

    Update index.ts

--- src/app/parishData/index.ts
+++ src/app/parishData/index.ts
@@ -1,76 +1,23 @@
 // src/app/parishData/index.ts
 
-export const parishData = [
-  {
-    id: "kingston",
-    name: "Kingston",
-    towns: ["New Kingston", "Half Way Tree", "Papine", "Downtown"],
-  },
-  {
-    id: "st-andrew",
-    name: "St. Andrew",
-    towns: ["Constant Spring", "Barbican", "Manor Park"],
-  },
-  {
-    id: "st-catherine",
-    name: "St. Catherine",
-    towns: ["Spanish Town", "Portmore", "Old Harbour"],
-  },
-  {
-    id: "clarendon",
-    name: "Clarendon",
-    towns: ["May Pen", "Chapelton", "Lionel Town"],
-  },
-  {
-    id: "manchester",
-    name: "Manchester",
-    towns: ["Mandeville", "Kendal", "Porus"],
-  },
-  {
-    id: "st-elizabeth",
-    name: "St. Elizabeth",
-    towns: ["Black River", "Santa Cruz", "Junction"],
-  },
-  {
-    id: "westmoreland",
-    name: "Westmoreland",
-    towns: ["Savanna-la-Mar", "Negril", "Little London"],
-  },
-  {
-    id: "hanover",
-    name: "Hanover",
-    towns: ["Lucea", "Hopewell", "Green Island"],
-  },
-  {
-    id: "st-james",
-    name: "St. James",
-    towns: ["Montego Bay", "Rose Hall", "Cambridge"],
-  },
-  {
-    id: "trelawny",
-    name: "Trelawny",
-    towns: ["Falmouth", "Duncans", "Wakefield"],
-  },
-  {
-    id: "st-ann",
-    name: "St. Ann",
-    towns: ["Ocho Rios", "St. Ann's Bay", "Runaway Bay"],
-  },
-  {
-    id: "st-mary",
-    name: "St. Mary",
-    towns: ["Port Maria", "Oracabessa", "Annotto Bay"],
-  },
-  {
-    id: "portland",
-    name: "Portland",
-    towns: ["Port Antonio", "Buff Bay", "Hope Bay"],
-  },
-  {
-    id: "st-thomas",
-    name: "St. Thomas",
-    towns: ["Morant Bay", "Yallahs", "Seaforth"],
-  },
-];
+export interface ParishInfo {
+  name: string;
+  towns: string[];
+}
 
-export default parishData;
+export const parishes: ParishInfo[] = [
+  { name: "Kingston", towns: ["Downtown", "New Kingston", "Half Way Tree"] },
+  { name: "St. Andrew", towns: ["Constant Spring", "Red Hills", "Papine"] },
+  { name: "St. Catherine", towns: ["Spanish Town", "Portmore", "Linstead"] },
+  { name: "Clarendon", towns: ["May Pen", "Chapelton", "Frankfield"] },
+  { name: "Manchester", towns: ["Mandeville", "Christiana", "Porus"] },
+  { name: "St. Elizabeth", towns: ["Black River", "Santa Cruz", "Lacovia"] },
+  { name: "Westmoreland", towns: ["Savanna-la-Mar", "Negril", "Grange Hill"] },
+  { name: "Hanover", towns: ["Lucea", "Hopewell", "Sandy Bay"] },
+  { name: "St. James", towns: ["Montego Bay", "Rose Hall", "Anchovy"] },
+  { name: "Trelawny", towns: ["Falmouth", "Duncans", "Clark’s Town"] },
+  { name: "St. Ann", towns: ["Ocho Rios", "St. Ann’s Bay", "Runaway Bay"] },
+  { name: "St. Mary", towns: ["Port Maria", "Annotto Bay", "Highgate"] },
+  { name: "Portland", towns: ["Port Antonio", "Buff Bay", "Long Bay"] },
+  { name: "St. Thomas", towns: ["Morant Bay", "Yallahs", "Seaforth"] },
+];
