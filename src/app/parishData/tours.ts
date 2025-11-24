a19679a1b859ae2d8eaa2b2253c32f2590cbada2
RootsRaddik6 <korjamltd@gmail.com>
2025-11-24 00:18:36 -0500

    Delete src/app/parishData directory

--- src/app/parishData/tours.ts
+++
@@ -1,5 +0,0 @@
-export const TOURS = [
-  { parishCode: "STB", name: "Dunn's River Tour" },
-  { parishCode: "PTL", name: "Blue Lagoon Tour" },
-  { parishCode: "SJB", name: "Rose Hall Great House" }
-];
