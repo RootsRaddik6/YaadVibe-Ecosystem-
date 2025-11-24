763eb7c5a01421d0c22123454af0e8591f8fdd0a
RootsRaddik6 <korjamltd@gmail.com>
2025-11-24 08:13:34 -0500

    Delete src/utils directory

--- src/utils/ton.ts
+++
@@ -1,7 +0,0 @@
-import { TonClient, WalletContractV4 } from "ton";
-import { mnemonicToPrivateKey } from "ton-crypto";
-
-export const tonClient = new TonClient({
-  endpoint: "https://testnet.toncenter.com/api/v2/jsonRPC",
-  apiKey: process.env.TON_API_KEY
-});
