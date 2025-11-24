6dddb3e1e3092b1250271d2b84a9df18f4c1ce0a
RootsRaddik6 <korjamltd@gmail.com>
2025-11-23 21:24:44 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,5 @@
-
+function subscribe() {
+  const msg = document.getElementById("subMessage");
+  msg.innerText = "Thanks! You’ll be notified when the full YaadVibe experience launches.";
+  msg.style.color = "#00d66c";
+}
