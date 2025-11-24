9c02206fbf09329126c78e9a79b931a6c6033e85
RootsRaddik6 <korjamltd@gmail.com>
2025-11-24 08:14:47 -0500

    Delete utils directory

--- utils/prices.ts
+++
@@ -1,7 +0,0 @@
-export const defaultPrices = {
-  transport: 120,
-  hotelPerNight: 180,
-  foodPerGuest: 40,
-  attractionPerGuest: 60,
-  fee: 0.02,
-};
--- utils/sbt.ts
+++
@@ -1,8 +0,0 @@
-export async function mintSBT(wallet: string, booking: any) {
-  return {
-    ok: true,
-    sbt: "SBT_MINTED",
-    wallet,
-    data: booking,
-  };
-}
