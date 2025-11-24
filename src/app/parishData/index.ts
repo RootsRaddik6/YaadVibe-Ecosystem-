3ae4a723cd0739d8aa82b6c4bc21c916f2edc241
RootsRaddik6 <korjamltd@gmail.com>
2025-11-23 21:51:18 -0500

    Update index.ts

--- src/app/parishData/index.ts
+++ src/app/parishData/index.ts
@@ -1,8 +1,76 @@
-const parishData = {
-  hotels: [],
-  attractions: [],
-  tours: [],
-  transport: [],
-};
+// src/app/parishData/index.ts
+
+export const parishData = [
+  {
+    id: "kingston",
+    name: "Kingston",
+    towns: ["New Kingston", "Half Way Tree", "Papine", "Downtown"],
+  },
+  {
+    id: "st-andrew",
+    name: "St. Andrew",
+    towns: ["Constant Spring", "Barbican", "Manor Park"],
+  },
+  {
+    id: "st-catherine",
+    name: "St. Catherine",
+    towns: ["Spanish Town", "Portmore", "Old Harbour"],
+  },
+  {
+    id: "clarendon",
+    name: "Clarendon",
+    towns: ["May Pen", "Chapelton", "Lionel Town"],
+  },
+  {
+    id: "manchester",
+    name: "Manchester",
+    towns: ["Mandeville", "Kendal", "Porus"],
+  },
+  {
+    id: "st-elizabeth",
+    name: "St. Elizabeth",
+    towns: ["Black River", "Santa Cruz", "Junction"],
+  },
+  {
+    id: "westmoreland",
+    name: "Westmoreland",
+    towns: ["Savanna-la-Mar", "Negril", "Little London"],
+  },
+  {
+    id: "hanover",
+    name: "Hanover",
+    towns: ["Lucea", "Hopewell", "Green Island"],
+  },
+  {
+    id: "st-james",
+    name: "St. James",
+    towns: ["Montego Bay", "Rose Hall", "Cambridge"],
+  },
+  {
+    id: "trelawny",
+    name: "Trelawny",
+    towns: ["Falmouth", "Duncans", "Wakefield"],
+  },
+  {
+    id: "st-ann",
+    name: "St. Ann",
+    towns: ["Ocho Rios", "St. Ann's Bay", "Runaway Bay"],
+  },
+  {
+    id: "st-mary",
+    name: "St. Mary",
+    towns: ["Port Maria", "Oracabessa", "Annotto Bay"],
+  },
+  {
+    id: "portland",
+    name: "Portland",
+    towns: ["Port Antonio", "Buff Bay", "Hope Bay"],
+  },
+  {
+    id: "st-thomas",
+    name: "St. Thomas",
+    towns: ["Morant Bay", "Yallahs", "Seaforth"],
+  },
+];
 
 export default parishData;
