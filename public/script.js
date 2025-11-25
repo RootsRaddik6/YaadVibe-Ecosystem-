e1ee4dec7af09dcb391a7f439ecb3be7f1624f71
RootsRaddik6 <korjamltd@gmail.com>
2025-11-25 17:18:23 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -5,3 +5,15 @@ function openTon() {
 function openWalletConnect() {
     window.location.href = "https://walletconnect.com/";
 }
+// 3D holographic tilt for flag
+const flagWrapper = document.querySelector('.flag-3d-wrapper');
+const holoLayer = document.querySelector('.flag.holo-layer');
+
+if (flagWrapper && holoLayer) {
+    flagWrapper.addEventListener('mousemove', (e) => {
+        const rect = flagWrapper.getBoundingClientRect();
+        const x = (e.clientX - rect.left) / rect.width - 0.5;
+
+        holoLayer.style.transform = `translateZ(40px) rotateY(${x * 16}deg)`;
+    });
+}
