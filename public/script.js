a228c137b845ebaa87bb0a8a5b2845bab0795216
RootsRaddik6 <korjamltd@gmail.com>
2025-11-25 17:48:19 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -17,3 +17,20 @@ if (flagWrapper && holoLayer) {
         holoLayer.style.transform = `translateZ(40px) rotateY(${x * 16}deg)`;
     });
 }
+function openTon() {
+    window.location.href = "https://ton.org/en/wallets";
+}
+
+function openWalletConnect() {
+    window.location.href = "https://walletconnect.com/";
+}
+
+// 3D holographic tilt for the HERO card
+const hero3d = document.getElementById('hero3d');
+
+document.addEventListener('mousemove', (e) => {
+    const x = (window.innerWidth / 2 - e.clientX) / 25;
+    const y = (window.innerHeight / 2 - e.clientY) / 25;
+
+    hero3d.style.transform = `rotateY(${-x}deg) rotateX(${y}deg)`;
+});
